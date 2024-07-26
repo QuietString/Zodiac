549b01b849c53f86a3265be80abd257d9727038e
QuietString <the.quiet.string@gmail.com>
2024-07-26 12:37:13 +0900

    5.4 Initial

---
+++ Source/Zodiac/ZodiacGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ZodiacGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AZodiacGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AZodiacGameMode();
+};
+
+
+
